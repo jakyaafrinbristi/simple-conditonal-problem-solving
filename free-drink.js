5ad498f2c71c2f83236c88748b64ac547596f08f
jakyaafrinbristi <jakyaafrinb@gmail.com>
2024-02-01 13:20:02 +0600

    free drink

---
+++ free-drink.js
@@ -0,0 +1,15 @@
+/***
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+var burgerPrice = 60;
+
+if(burgerPrice >=500){
+    console.log("free coke")
+}
+else{
+    console.log("30 taka diye coke kinte hobe")
+}
